9f26db48413a3e41d06f3b20ba666de653ac8db8
camiloeferrera <camiloferrera@hotmail.com>
2020-02-14 16:06:05 -0600

    Hoy si se trabajo en el CRUD el anterior commit fue un error JAJAJA

--- Lab#4_SebastianRamirez&CamiloFerrera/src/lab/pkg4_sebastianramirez/camiloferrera/Lab4_SebastianRamirezCamiloFerrera.java
+++ Lab#4_SebastianRamirez&CamiloFerrera/src/lab/pkg4_sebastianramirez/camiloferrera/Lab4_SebastianRamirezCamiloFerrera.java
@@ -28,7 +28,7 @@ public class Lab4_SebastianRamirezCamiloFerrera {
         }
     }
     
-    static void CRUD (int x) throws Exception{
+    static void CRUD (int x){
         System.out.println("1. Crear \n"
                 + "2. Modificar \n"
                 + "3. Eliminar \n"
@@ -44,14 +44,17 @@ public class Lab4_SebastianRamirezCamiloFerrera {
                         String casa = SeleccionarCasa();
                         boolean validarcasa = ValidarCasa(casa);
                         while (validarcasa == false) {
-                            Excepcion e = new Excepcion();
-                            e.Casa();
+                            System.out.println();
+                            System.out.println("Ya hay un equipo con esta casa, seleccione otra.");
+                            System.out.println();
+                            casa = SeleccionarCasa();
+                            validarcasa = ValidarCasa(casa);
                         }
                         equipos.add(new Equipo (casa,0,0));
                         System.out.println();
-                        System.out.println("Equipo Añadido Exitosamente");
+                        System.out.println("Equipo Añadido Exitosamente!");
                     } else {
-                        System.out.println("Ya existen 4 equipos, con su respectiva casa");
+                        System.out.println("Ya existen 4 equipos, con su respectiva casa.");
                     }
                     break;
                 case "2":
@@ -72,7 +75,48 @@ public class Lab4_SebastianRamirezCamiloFerrera {
         } else {
             switch (opcion) {
                 case "1":
-                    
+                    if (equipos.size() == 0){
+                        System.out.println("No hay equipos creados, por lo tanto no se pueden crear jugadores");
+                    } else {
+                        System.out.print("Ingrese nombre: ");
+                        sc.nextLine();
+                        String nombre = sc.nextLine();
+                        System.out.print("Ingrese años cursando en Hogwarts: ");
+                        int años = sc.nextInt();
+                        System.out.print("Ingrese numero de uniforme: ");
+                        int numuniforme = sc.nextInt();
+                        System.out.println();
+                        String casa = SeleccionarCasa();                     
+                        System.out.println();
+                        
+                        System.out.println("1. Guardián \n"
+                                + "2. Golpeador \n"
+                                + "3. Cazador \n"
+                                + "4. Buscador");
+                        System.out.print("Seleccione opción para su posición: ");
+                        int opcionpos = sc.nextInt();
+                        
+                        while (opcionpos < 1 || opcionpos > 4) {
+                            System.out.println();
+                            System.out.println("Posicion no encontrada, intente de nuevo");
+                            System.out.println();
+                            System.out.println("1. Guardián \n"
+                                + "2. Golpeador \n"
+                                + "3. Cazador \n"
+                                + "4. Buscador");
+                            System.out.print("Seleccione opción para su posición: ");
+                            opcionpos = sc.nextInt();
+                        }
+                        System.out.println();
+                        
+                        if (opcionpos == 1){
+                            
+                        }
+                    }
+                        
+                    break;
+                case "3":
+                    Eliminar(jugadores);
                     break;
                 default:
                     System.out.println("Opción Incorrecta, regresando al menu principal...");
@@ -164,6 +208,7 @@ public class Lab4_SebastianRamirezCamiloFerrera {
         System.out.print("Seleccione equipo a modificar: ");
         int opcionequipo = sc.nextInt();
         opcionequipo--;
+        
         while (opcionequipo >= equipos.size() || opcionequipo < 0) {
             System.out.println();
             System.out.println("Equipo no encontrado, intende de nuevo");
@@ -208,13 +253,116 @@ public class Lab4_SebastianRamirezCamiloFerrera {
                 }
                 System.out.println();
                 equipos.get(opcionequipo).setCasa(casa);
-                System.out.println("Casa modificada exitosamente");
+                System.out.println("Casa modificada exitosamente!");
                 break;
             case 2:
+                for (int i = 0; i < jugadores.size(); i++) {
+                    System.out.println((i+1) + ". " + jugadores.get(i));
+                }
+                System.out.print("Seleccione opción de jugador a añadir: ");
+                opcion = sc.nextInt();
+                opcion--;
+                
+                while (opcion < 0 || opcion >= jugadores.size()) {
+                    System.out.println();
+                    System.out.println("Jugador no encontrado, intente de nuevo.");
+                    System.out.println();
+                    for (int i = 0; i < jugadores.size(); i++) {
+                        System.out.println((i+1) + ". " + jugadores.get(i));
+                    }
+                    System.out.print("Seleccione opción de jugador a añadir: ");
+                    opcion = sc.nextInt();
+                    opcion--;
+                }
+                System.out.println();
+                
+                boolean disponible = ValidarJugadoresEquipo(jugadores.get(opcion), equipos.get(opcionequipo).getJugadores());
+                if (disponible == false) {
+                    System.out.println("Ya hay suficientes jugadores en su posición, no se pudo añadir. ");
+                } else {
+                    equipos.get(opcionequipo).getJugadores().add(jugadores.get(opcion));
+                    System.out.println("Jugador añadido exitosamente!");
+                    equipos.get(opcionequipo).setPromedioagilidad();
+                    equipos.get(opcionequipo).setPromfuerza();
+                    equipos.get(opcionequipo).setPromvelocidad();
+                }
                 break;
             case 3:
+                if (equipos.get(opcionequipo).getJugadores().size() == 0){
+                    System.out.println("No hay jugadores en este equipo, por lo tanto no se puede elegir un capitan.");
+                } else {
+                    for (int i = 0; i < equipos.get(opcionequipo).getJugadores().size() ; i++) {
+                        System.out.println((i+1) + ". " + equipos.get(opcionequipo).getJugadores().get(i).getNombre());
+                    }
+                    System.out.print("Seleccione opción de jugador para capitán: ");
+                    int opcioncapitan = sc.nextInt();
+                    opcioncapitan--;
+                    
+                    while (opcioncapitan < 0 || opcioncapitan >= equipos.get(opcionequipo).getJugadores().size()) {
+                        System.out.println();
+                        System.out.println("Jugador no entontrado, intente de nuevo.");
+                        System.out.println();
+                        for (int i = 0; i < equipos.get(opcionequipo).getJugadores().size() ; i++) {
+                            System.out.println((i+1) + ". " + equipos.get(opcionequipo).getJugadores().get(i).getNombre());
+                        }
+                        System.out.print("Seleccione opción de jugador para capitán: ");
+                        opcioncapitan = sc.nextInt();
+                        opcioncapitan--;
+                    }
+                    System.out.println();
+                    
+                    equipos.get(opcionequipo).setCapitan(equipos.get(opcionequipo).getJugadores().get(opcioncapitan));
+                    System.out.println("Capitan seleccionado exitosamente!");
+                }
                 break;
         }
     }
     
+    static boolean ValidarJugadoresEquipo (Jugador x, ArrayList<Jugador> y){
+        boolean disponible = true;
+        int cont = 0;
+        if (x instanceof Guardian){
+            for (int i = 0; i < y.size() ; i++) {
+                if (y.get(i) instanceof Guardian){
+                    cont ++;
+                }
+            }
+            
+            if (cont == 1){
+                disponible =  false;
+            } 
+        } else if (x instanceof Golpeadores){
+            for (int i = 0; i < y.size() ; i++) {
+                if (y.get(i) instanceof Golpeadores){
+                    cont ++;
+                }
+            }
+            
+            if (cont == 2){
+                disponible = false;
+            }
+        } else if (x instanceof Cazadores){
+            for (int i = 0; i < y.size() ; i++) {
+                if (y.get(i) instanceof Cazadores){
+                    cont ++;
+                }
+            }
+            
+            if (cont == 3){
+                disponible =  false;
+            }
+        } else if (x instanceof Buscador){
+            for (int i = 0; i < y.size() ; i++) {
+                if (y.get(i) instanceof Buscador){
+                    cont ++;
+                }
+            }
+            
+            if (cont == 1){
+                disponible =  false;
+            } 
+        }
+        
+        return disponible;
+    }
 }
