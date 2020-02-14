aac22708d961fdc5f6b868e39d485a853432ef2c
Sebas-Ramirez <ramirez.sebaas@gmail.com>
2020-02-14 13:19:40 -0600

    Creacion Proyecto

---
+++ Lab#4_SebastianRamirez&CamiloFerrera/src/lab/pkg4_sebastianramirez/camiloferrera/Lab4_SebastianRamirezCamiloFerrera.java
@@ -0,0 +1,7 @@
+package lab.pkg4_sebastianramirez.camiloferrera;
+public class Lab4_SebastianRamirezCamiloFerrera {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
